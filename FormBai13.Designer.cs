febcdab6262866a4bee072905ca296841ec7f5f5
QuangNhat-2005 <caoquangnhat993@gmail.com>
2025-12-15 22:16:22 +0700

    Hoan Thanh Bai 13

---
+++ FormBai13.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CaoQuangNhat_2123110077_1_
+{
+    partial class FormBai13
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cb_Faculty = new ComboBox();
+            tbDisplay = new TextBox();
+            btClear = new Button();
+            btOK = new Button();
+            SuspendLayout();
+            // 
+            // cb_Faculty
+            // 
+            cb_Faculty.FormattingEnabled = true;
+            cb_Faculty.Location = new Point(172, 54);
+            cb_Faculty.Name = "cb_Faculty";
+            cb_Faculty.Size = new Size(402, 33);
+            cb_Faculty.TabIndex = 0;
+            // 
+            // tbDisplay
+            // 
+            tbDisplay.Location = new Point(172, 114);
+            tbDisplay.Multiline = true;
+            tbDisplay.Name = "tbDisplay";
+            tbDisplay.Size = new Size(402, 209);
+            tbDisplay.TabIndex = 1;
+            // 
+            // btClear
+            // 
+            btClear.Location = new Point(326, 341);
+            btClear.Name = "btClear";
+            btClear.Size = new Size(112, 34);
+            btClear.TabIndex = 2;
+            btClear.Text = "Clear";
+            btClear.UseVisualStyleBackColor = true;
+            btClear.Click += btClear_Click;
+            // 
+            // btOK
+            // 
+            btOK.Location = new Point(462, 341);
+            btOK.Name = "btOK";
+            btOK.Size = new Size(112, 34);
+            btOK.TabIndex = 3;
+            btOK.Text = "OK";
+            btOK.UseVisualStyleBackColor = true;
+            btOK.Click += btOK_Click;
+            // 
+            // FormBai13
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(773, 450);
+            Controls.Add(btOK);
+            Controls.Add(btClear);
+            Controls.Add(tbDisplay);
+            Controls.Add(cb_Faculty);
+            Name = "FormBai13";
+            Text = "FormBai13";
+            Load += FormBai13_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cb_Faculty;
+        private TextBox tbDisplay;
+        private Button btClear;
+        private Button btOK;
+    }
+}
---
+++ FormBai13.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections; // Dùng cho ArrayList
+using System.Windows.Forms;
+
+namespace CaoQuangNhat_2123110077_1_
+{
+    // --- LƯU Ý QUAN TRỌNG: Class Form phải nằm trên cùng để mở được giao diện ---
+    public partial class FormBai13 : Form
+    {
+        public FormBai13()
+        {
+            InitializeComponent();
+        }
+
+        // 1. Hàm tạo dữ liệu giả (Theo Slide 100)
+        public ArrayList GetData()
+        {
+            ArrayList lst = new ArrayList();
+
+            Faculty f = new Faculty();
+            f.Id = "K01";
+            f.Name = "Công nghệ thông tin";
+            f.Quantity = 1200;
+            lst.Add(f);
+
+            f = new Faculty();
+            f.Id = "K02";
+            f.Name = "Quản trị kinh doanh";
+            f.Quantity = 4200;
+            lst.Add(f);
+
+            f = new Faculty();
+            f.Id = "K03";
+            f.Name = "Kế toán tài chính";
+            f.Quantity = 5200;
+            lst.Add(f);
+
+            return lst;
+        }
+
+        // 2. Sự kiện Form Load (Khi mở lên)
+        private void FormBai13_Load(object sender, EventArgs e)
+        {
+            ArrayList lst = GetData();
+            cb_Faculty.DataSource = lst;
+            cb_Faculty.DisplayMember = "Name"; // Hiện Tên khoa ra ngoài
+        }
+
+        // 3. Sự kiện khi chọn thay đổi
+        private void cb_Faculty_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Khi chọn thì lấy Mã khoa (Id)
+            cb_Faculty.ValueMember = "Id";
+
+            // Kiểm tra null để tránh lỗi khi vừa reset
+            if (cb_Faculty.SelectedValue != null)
+            {
+                string id = cb_Faculty.SelectedValue.ToString();
+                tbDisplay.Text = "Bạn đã chọn khoa có mã: " + id;
+            }
+        }
+
+        // 4. Sự kiện bấm nút OK
+        private void btOK_Click(object sender, EventArgs e)
+        {
+            // Khi bấm OK thì lấy Tên khoa (Name)
+            cb_Faculty.ValueMember = "Name";
+
+            if (cb_Faculty.SelectedValue != null)
+            {
+                string name = cb_Faculty.SelectedValue.ToString();
+                tbDisplay.Text = "Bạn đã chọn khoa có tên: " + name;
+            }
+        }
+
+        // 5. Sự kiện bấm nút CLEAR (Mới thêm)
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            // Xóa chữ trên màn hình
+            tbDisplay.Text = "";
+
+            // Reset ComboBox về trạng thái chưa chọn gì cả (tùy chọn)
+            // cb_Faculty.SelectedIndex = -1; 
+
+            // Hoặc đưa con trỏ về ô nhập liệu (nếu có)
+            MessageBox.Show("Đã xóa màn hình!");
+        }
+    }
+
+    // --- Class Faculty nằm ở dưới cùng (Để không bị lỗi Designer) ---
+    public class Faculty
+    {
+        // Thêm giá trị mặc định = "" để tránh cảnh báo vàng (Null Warning)
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+        public int Quantity { get; set; }
+    }
+}
